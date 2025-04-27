38adf25fff84f53e7bf2129bd5b1b6f4f9d16ea9
SK NAIM AKHTAR <99519452+nakhtarcontai@users.noreply.github.com>
2025-04-27 22:00:56 +0530

    Add files via upload

---
+++ Module 2: Basic Python Concepts/Task1.py
@@ -0,0 +1,18 @@
+# Perform Basic Mathematical Operations
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+
+# output:
+# Enter first number: 10
+# Enter second number: 2
+# Addition: 12
+# Subtraction: 8
+# Multiplication: 20
+# Division: 5.0
