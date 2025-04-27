d000441787c9bca8fbcfd82ebc56c2ef5a9c131f
SK NAIM AKHTAR <99519452+nakhtarcontai@users.noreply.github.com>
2025-04-27 22:07:08 +0530

    Add files via upload

---
+++ Module 2: Basic Python Concepts/Task2.py
@@ -0,0 +1,11 @@
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+
+
+print("Hello, " + a + " " + b + "! Welcome to the python program") 
+
+
+# output:
+# Enter your first name: John
+# Enter your last name: Doe 
+# Hello, John Doe! Welcome to the python program
