ec41770bd5662dafbb68319b47039714e7001ed2
SK NAIM AKHTAR <99519452+nakhtarcontai@users.noreply.github.com>
2025-05-01 19:32:02 +0530

    Add files via upload

---
+++ Module 3: Control Structures in Python/even_odd_number.py
@@ -0,0 +1,15 @@
+# Task 1: Check if a Number is Even or Odd
+
+n =int(input("Enter a number: "))
+
+if(n%2==0):
+    print("Even")
+else:
+    print("Odd")
+
+
+# output:
+# Enter a number: 56
+# Even
+# Enter a number: 77
+# Odd
